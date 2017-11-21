19c35b09ff7944197d8cc0efa0d7db97e04a9f1a
Amber <ambersethi97@gmail.com>
2017-11-21 17:56:17 +0530

    Third

---
+++ app-swoosh/app-swoosh/Model/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  app-swoosh
+//
+//  Created by Amber Sethi on 21/11/17.
+//  Copyright © 2017 Amber Sethi. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
